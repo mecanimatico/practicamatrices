5a2a52a77c9acb83f2dc24f3d927b6901b37d94b
Heber Pareja Reinoso <mecanimatico@gmail.com>
2018-11-04 18:44:11 -0500

    Se realizaron comentarios a manera de ilustración

--- Operaciones con matrices/eigen3.py
+++ Operaciones con matrices/eigen3.py
@@ -3,11 +3,13 @@
 import numpy as np
 print("-----------------------------------------")
 print("Matriz A")
-
+"""Dada la matriz A, se hallan los valores y vectores propios
+"""
 A = np.array([[3,2],
 [7, -2]])
 print(A)
 print("-----------------------------------------")
 x,v =np.linalg.eig(A)
 print(x,v)
+print("Imprime valor y vector propio: ")
 print("------------------------------------------")
